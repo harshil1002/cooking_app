4de83157d4efb9caa2c926236e22364a4701e9e5
SkillQode <83206892+SkillQode@users.noreply.github.com>
2021-08-26 12:39:49 +0530

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:cooking_app/sign_in.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Admob.initialize();
   runApp(CookingApp());
 }
 
--- lib/welcome_aboard.dart
+++ lib/welcome_aboard.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:cooking_app/bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:admob_flutter/admob_flutter.dart';
+import 'package:flutter_native_admob/flutter_native_admob.dart';
+import 'package:flutter_native_admob/native_admob_controller.dart';
 
 class WelcomeAboard extends StatefulWidget {
   @override
@@ -7,6 +12,16 @@ class WelcomeAboard extends StatefulWidget {
 }
 
 class _WelcomeAboardState extends State<WelcomeAboard> {
+  static const _adUnitID = "ca-app-pub-3940256099942544/8135179316";
+
+  final _nativeAdController = NativeAdmobController();
+
+  @override
+  void initState() {
+    super.initState();
+    Admob.requestTrackingAuthorization();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,24 +42,32 @@ class _WelcomeAboardState extends State<WelcomeAboard> {
                   ),
                 ),
                 child: Container(
-                    height: 816,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          'assets/images/dark_bg.png',
-                        ),
-                        fit: BoxFit.cover,
+                  height: 816,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/dark_bg.png',
                       ),
+                      fit: BoxFit.cover,
                     ),
+                  ),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        'Welcome to',
+                        'Welcome to.',
                         style: TextStyle(
-                          color: Color(0xffFFFEFE),
+                          color: Color(0xffFFFFFF),
                           fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 8.0,
+                              color: Colors.black,
+                              offset: Offset(4.0, 4.0),
+                            ),
+                          ],
                         ),
                       ),
                       Text(
@@ -66,52 +89,94 @@ class _WelcomeAboardState extends State<WelcomeAboard> {
                   ),
                 ),
               ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 500),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => BottombarBar(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      height: 60,
-                      width: 260,
-                      decoration: BoxDecoration(
-                        color: Color(0xffE1C427),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(11),
+              SafeArea(
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.9,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 300,
+                        width: double.infinity,
+                        color: Colors.white,
+                        child: NativeAdmob(
+                          adUnitID: 'ca-app-pub-3940256099942544/8135179316',
+                          numberAds: 3,
+                          controller: _nativeAdController,
+                          type: NativeAdmobType.full,
                         ),
                       ),
-                      child: Center(
-                        child: Text(
-                          'START COOKING',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            shadows: [
-                              Shadow(
-                                blurRadius: 30.0,
-                                color: Colors.black,
-                                offset: Offset(4.0, 4.0),
+                      Container(
+                        width: double.infinity,
+                        color: Colors.white,
+                        child: AdmobBanner(
+                          adUnitId: getBannerAdUnitId(),
+                          adSize: AdmobBannerSize.BANNER,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 500),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => BottombarBar(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          height: 60,
+                          width: 260,
+                          decoration: BoxDecoration(
+                            color: Color(0xffE1C427),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(11),
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'START COOKING',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                shadows: [
+                                  Shadow(
+                                    blurRadius: 30.0,
+                                    color: Colors.black,
+                                    offset: Offset(4.0, 4.0),
+                                  ),
+                                ],
                               ),
-                            ],
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
             ],
           ),
         ],
       ),
     );
+  }
+
+  String getBannerAdUnitId() {
+    if (Platform.isIOS) {
+      return 'ca-app-pub-3940256099942544/2934735716';
+    } else if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/6300978111';
+    }
+    return null;
   }
 }
